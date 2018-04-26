9678b8b19966f7f4e61c6e0ee7acb20cd26a4889
DONGBUM, KIM <dongbum9@gmail.com>
2018-04-26 15:34:30 +0900

    websocket 라이브러리를 사용하지 않고 socket.io 를 사용하도록 수정

--- app.js
+++ app.js
@@ -1,66 +1,44 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var fs = require('fs');
 
-var httpServer = http.createServer(function(req, res) {
-  fs.readFile('websocket.html', function (error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.end();
-      return;
-    }
-
-    res.writeHead(200);
-    res.write(data);
-    res.end();
-  });
+app.get("/", function(req, res) {
+  res.sendfile('websocket.html');
 });
 
-httpServer.listen(58080, function () {
+http.listen('58080', function() {
   console.log('http server is listening on port 58080');
 });
 
-// 웹소켓 서버
-var WebSocketServer = require('websocket').server;
-var wsserver = new WebSocketServer({
-  httpServer: httpServer,
-  autoAcceptConnections: false
-});
+// require('events').EventEmitter.prototype._maxListeners = 100;
 
-require('events').EventEmitter.prototype._maxListeners = 100;
+io.on('connection', function (socket) {
+  console.log('user connected');
 
-wsserver.on('request', function (request) {
-  var connection = request.accept('example-echo', request.origin);
-  connection.on('message', function (message) {
-    //utf8인지 바이너리 인지
-    //메세지 형식이 utf8
-    /*
-    if (message.type === 'utf8') {
-      console.log('Received message: ' + message.utf8Data);
-      connection.sendUTF(message.utf8Data);
-    }
-    //바이너리 인지
-    else if (message.type === 'binary') {
-      connection.sendBytes(message.binaryData);
-    }
-    */
+  socket.on('start', function() {
     tail.on("line", function (data) {
-        connection.sendUTF(data);
+        console.log(data);
+        io.emit('log', data);
     });
   });
 
-  connection.on('close', function (reasonCode, description) {
-    console.log('Peer ' + connection.remoteAddress + ' disconnected.');
+  socket.on('disconnect', function () {
+    console.log('disconnected.');
   });
 });
 
 
-var fileToTail = "D:\\nodetest\\NodejsConsoleApp1\\test.txt";
-var options = { logger: console, fromBeginning: false, follow: true, useWatchFile: true, fsWatchOptions : { interval: 10 } }
+var fileToTail = "test.txt";
+var options = { logger: console, fromBeginning: false, follow: true, useWatchFile: true, fsWatchOptions : { interval: 100 } }
+// 위 옵션을 100 미만으로 설정하는 경우  tail 기능이 제대로 작동하지 않을 우려가 있다.
 
 Tail = require('tail').Tail;
 
 tail = new Tail(fileToTail, options);
 
+/*
 tail.on("line", function (data) {
     console.log(data);
 });
@@ -68,3 +46,4 @@ tail.on("line", function (data) {
 tail.on("error", function (error) {
     console.log('ERROR: ', error);
 });
+*/
