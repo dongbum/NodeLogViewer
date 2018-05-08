e4b2eb10a61fa4d4b60e87c8805572bd4f0a94d7
DONGBUM, KIM <dongbum9@gmail.com>
2018-05-08 14:33:47 +0900

    로그 디렉토리에서 특정 확장자만 처리할 수 있도록 수정

--- app.js
+++ app.js
@@ -17,12 +17,23 @@ for (var i=0; i<LOG_FILE.length; i++) {
 for (var i=0; i<LOG_DIR.length; i++) {
   console.log(LOG_DIR[i].name + " : " + LOG_DIR[i].dir);
 
+  var filters = LOG_DIR[i].filter.split('|'); // 로그 파일 필터
+
   var logfiles = fs.readdirSync(LOG_DIR[i].dir);
   for (var j in logfiles) {
-    var obj = {"id":LOG_DIR[i].id + "-" + logfiles[j], "file":LOG_DIR[i].dir + "\\" + logfiles[j]};
-    LOG_FILE.push(obj);
+    var logfile_name = logfiles[j];
 
-    console.log(obj);
+    for (var k in filters) { // 로그 파일 필터와 비교하여 맞는 확장자만 처리한다.
+      if ('*' == filters[k]) {
+        var obj = {"id":LOG_DIR[i].id + "-" + logfile_name, "file":LOG_DIR[i].dir + "\\" + logfile_name};
+        LOG_FILE.push(obj);
+        console.log(obj);
+      } else if (logfile_name.split('.').pop() == filters[k]) {
+        var obj = {"id":LOG_DIR[i].id + "-" + logfile_name, "file":LOG_DIR[i].dir + "\\" + logfile_name};
+        LOG_FILE.push(obj);
+        console.log(obj);
+      }
+    }
   }
 }
 
